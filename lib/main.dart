b0293c129ef190f0c75747a1a1fa0701cfde5630
Essra <yokiyori@gmail.com>
2023-07-04 09:09:22 +0300

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -14,24 +15,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(),
-      body: Text("HomePage"),
-    );
-  }
-}
-
-class LogInPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), drawer: Drawer(), body: Center());
-  }
-}
-
 class Test extends StatefulWidget {
   State<StatefulWidget> createState() {
     return TestState();
@@ -39,54 +22,76 @@ class Test extends StatefulWidget {
 }
 
 class TestState extends State<Test> {
-  GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+  @override
   Widget build(BuildContext context) {
-    List user = ["essra", "Arwa", "Asma"];
     return Scaffold(
-        key: scaffoldKey,
-        appBar: AppBar(
-              title: Text("Home Page"),
-              actions: [
-                  IconButton(icon:Icon(Icons.ac_unit),onPressed:(){
-                    scaffoldKey.currentState?.openEndDrawer();
-                  })
-              ],
-        ),
-        drawerScrimColor:Colors.transparent,
-        endDrawer: Drawer(
-          child: Column(children: [
-            UserAccountsDrawerHeader(
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Colors.red,
-                  child: Text("E"),
-                ),
-                accountName: Text("Essra"),
-                accountEmail: Text("essraalghamdi@gmail.com")),
-            ListTile(
-                title: Text("Home Page"),
-                leading: Icon(Icons.home),
-                onTap: () {}),
-            ListTile(
-              title: Text("Help"),
-              leading: Icon(Icons.help),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text("LogOut"),
-              leading: Icon(Icons.logout),
-              onTap: () {},
-            ),
-          ]),
-        ),
-        body: Center(
-          child: Container(
-          padding: EdgeInsets.all(3),
-          child: ElevatedButton(
-            onPressed: () {
-              scaffoldKey.currentState?.openEndDrawer();
-            },
-            child: Text("open drawer"),
-          ),
-        )));
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              icon:Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: DataSearch());
+              })
+        ],
+        title: Text("Home Page"),
+      ),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate {
+  List names = ["essra", "asma", "arwa", "azoz"];
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    //ترجع ليست
+    return [
+      IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {
+            //معرفة مسبقا في السيرش كلاس
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    //ترجع ويدجت
+    //تجي قبل في البداية
+    return IconButton(onPressed:(){},icon:Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Text("$query");
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    //ترجع ويدجت
+    List filternames =
+        names.where((element) => element.startsWith(query)).toList();
+    //اذا استخدمت contains
+    //حيجيب لي الأسماء الي تحتوي حرف معين
+    return ListView.builder(
+        itemCount: query == "" ? names.length : filternames.length,
+        itemBuilder: (context, i) {
+          return InkWell(
+              onTap: () {
+                query = query == "" ? names[i] : filternames[i];
+                showResults(context);
+              },
+              child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: query == ""
+                      ?Text(
+                          "${names[i]}",
+                          style: TextStyle(fontSize: 25),
+                        )
+                      : Text(
+                          "${filternames[i]}",
+                          style: TextStyle(fontSize: 25),
+                        )));
+        });
   }
 }
